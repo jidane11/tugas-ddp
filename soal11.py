9f4d5582ef157e1b01b07cbed14bb35da1aad4a6
jidane11 <164849806+jidane11@users.noreply.github.com>
2025-10-23 15:36:56 +0700

    Add files via upload

---
+++ soal11.py
@@ -0,0 +1,8 @@
+kendaraan = ["namaKendaraan", "jenisKendaraan","ccKendaraan","warnaKendaraan","rodaKendaraan" ]
+
+
+kendaraan.extend (["hargaKendaraan", "tipeKendaraan"])
+
+kendaraan.insert (2, "merkKendaraan")
+
+print(kendaraan)
